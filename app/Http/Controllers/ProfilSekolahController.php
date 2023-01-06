ea1a9a5286f52a2a7e2ca94d8dad8c96416d55ef
Abdul Majid Refindo <abdulmajirefindo@gmail.com>
2023-01-06 23:53:33 +0700

    initialize

---
+++ app/Http/Controllers/ProfilSekolahController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProfilSekolah;
+use App\Http\Requests\StoreProfilSekolahRequest;
+use App\Http\Requests\UpdateProfilSekolahRequest;
+
+class ProfilSekolahController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreProfilSekolahRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreProfilSekolahRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ProfilSekolah  $profilSekolah
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProfilSekolah $profilSekolah)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ProfilSekolah  $profilSekolah
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProfilSekolah $profilSekolah)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateProfilSekolahRequest  $request
+     * @param  \App\Models\ProfilSekolah  $profilSekolah
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateProfilSekolahRequest $request, ProfilSekolah $profilSekolah)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ProfilSekolah  $profilSekolah
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProfilSekolah $profilSekolah)
+    {
+        //
+    }
+}
