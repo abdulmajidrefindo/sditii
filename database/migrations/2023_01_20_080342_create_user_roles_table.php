7ba094b3e55b45bd541cbbe279ae0fb67e75fbb0
Abdul Majid Refindo <abdulmajirefindo@gmail.com>
2023-02-02 21:04:16 +0700

    update migration schema

--- database/migrations/2023_01_20_080342_create_user_roles_table.php
+++ database/migrations/2023_01_20_080342_create_user_roles_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
     public function up()
     {
         Schema::create('user_roles', function (Blueprint $table) {
-            $table->id();
             $table->foreignId('user_id');
             $table->foreignId('role_id');
             $table->timestamps();
