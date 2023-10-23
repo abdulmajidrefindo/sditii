e064c278cf993d14de30c477e7636681f5e3822f
Hernando Dio Palma <hernando.diopalma@gmail.com>
2023-10-23 09:00:28 +0700

    Add: Sistem Hak Akses Halaman

--- routes/web.php
+++ routes/web.php
@@ -27,59 +27,64 @@ use App\Http\Controllers\RaporSiswaController;
 */
 // Route::post('/dataUser', UserController::class, 'edit');
 // Route::get('/dataUser', UserController::class, 'edit');
-Route::resource('/dataUser', UserController::class);
-Route::resource('/dataGuru', GuruController::class);
-Route::resource('/dataSiswa', SiswaController::class);
-Route::resource('/dataTahunPelajaran', PeriodeController::class);
 
-Route::get('/getTableUser', [UserController::class, 'getTable'])->name('user.getTable');
-Route::get('/getTableGuru', [GuruController::class, 'getTable'])->name('guru.getTable');
-Route::get('/getTableSiswa', [SiswaController::class, 'getTable'])->name('siswa.getTable');
-Route::get('/getTablePeriode', [PeriodeController::class, 'getTable'])->name('periode.getTable');
+Route::middleware('auth')->group(function () {
 
+    Route::resource('/dataUser', UserController::class)->middleware('role:Guru,Administrator');
+    Route::resource('/dataGuru', GuruController::class);
+    Route::resource('/dataSiswa', SiswaController::class);
+    Route::resource('/dataTahunPelajaran', PeriodeController::class);
+    
+    Route::get('/getTableUser', [UserController::class, 'getTable'])->name('user.getTable');
+    Route::get('/getTableGuru', [GuruController::class, 'getTable'])->name('guru.getTable');
+    Route::get('/getTableSiswa', [SiswaController::class, 'getTable'])->name('siswa.getTable');
+    Route::get('/getTablePeriode', [PeriodeController::class, 'getTable'])->name('periode.getTable');
+    
+    
+    Route::get('/profilSekolah', [ProfilSekolahController::class, 'index']);
+    Route::get('/pengumuman', [PengumumanController::class, 'index']);
+    Route::get('/ibadahHarian', [SiswaIbadahHarianController::class, 'index']);
+    Route::get('/tahfidz', [SiswaTahfidzController::class, 'index']);
+    Route::get('/hadist', [SiswaHadistController::class, 'index']);
+    Route::get('/doa', [SiswaDoaController::class, 'index']);
+    Route::get('/iwr', [SiswaIlmanWaaRuuhanController::class, 'index']);
+    Route::get('/bidangStudi', [SiswaBidangStudiController::class, 'choose']);
+    Route::post('/bidangStudi', [SiswaBidangStudiController::class, 'index']);
+    Route::get('/raporSiswa', [RaporSiswaController::class, 'index']);
+    // Route::post('/raporSiswa', [RaporSiswaController::class, 'index']);
+    
+    Route::resource('/dataProfilSekolah', ProfilSekolahController::class);
+    Route::resource('/dataPengumuman', PengumumanController::class);
+    
+    
+    
+    Route::resource('/dataBidangStudi', SiswaBidangStudiController::class);
+    Route::resource('/dataRaporSiswa', RaporSiswaController::class);
+    
+    Route::resource('/siswaDoa', SiswaDoaController::class);
+    Route::resource('/siswaTahfidz', SiswaTahfidzController::class);
+    Route::resource('/siswaHadist', SiswaHadistController::class);
+    Route::resource('/siswaIbadahHarian', SiswaIbadahHarianController::class);
+    Route::resource('/siswaBidangStudi', SiswaBidangStudiController::class);
+    Route::resource('/siswaIlmanWaaRuuhan', SiswaIlmanWaaRuuhanController::class);
+    // choose kelas for siswaDoa
+    
+    
+    Route::get('/tes', function () {
+        return view('dashboard');
+    });
+    Route::get('/', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
+    Route::get('/register', function () {
+        return view('register');
+    });
+    
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');    
 
-Route::get('/profilSekolah', [ProfilSekolahController::class, 'index']);
-Route::get('/pengumuman', [PengumumanController::class, 'index']);
-Route::get('/ibadahHarian', [SiswaIbadahHarianController::class, 'index']);
-Route::get('/tahfidz', [SiswaTahfidzController::class, 'index']);
-Route::get('/hadist', [SiswaHadistController::class, 'index']);
-Route::get('/doa', [SiswaDoaController::class, 'index']);
-Route::get('/iwr', [SiswaIlmanWaaRuuhanController::class, 'index']);
-Route::get('/bidangStudi', [SiswaBidangStudiController::class, 'choose']);
-Route::post('/bidangStudi', [SiswaBidangStudiController::class, 'index']);
-Route::get('/raporSiswa', [RaporSiswaController::class, 'index']);
-// Route::post('/raporSiswa', [RaporSiswaController::class, 'index']);
-
-Route::resource('/dataProfilSekolah', ProfilSekolahController::class);
-Route::resource('/dataPengumuman', PengumumanController::class);
-
-
-
-Route::resource('/dataBidangStudi', SiswaBidangStudiController::class);
-Route::resource('/dataRaporSiswa', RaporSiswaController::class);
-
-Route::resource('/siswaDoa', SiswaDoaController::class);
-Route::resource('/siswaTahfidz', SiswaTahfidzController::class);
-Route::resource('/siswaHadist', SiswaHadistController::class);
-Route::resource('/siswaIbadahHarian', SiswaIbadahHarianController::class);
-Route::resource('/siswaBidangStudi', SiswaBidangStudiController::class);
-Route::resource('/siswaIlmanWaaRuuhan', SiswaIlmanWaaRuuhanController::class);
-// choose kelas for siswaDoa
-
-
-Route::get('/tes', function () {
-    return view('dashboard');
 });
-Route::get('/', function () {
-    return view('dashboard');
-});
-Route::get('/register', function () {
-    return view('register');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 // Route::middleware('auth')->group(function () {
 //     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
