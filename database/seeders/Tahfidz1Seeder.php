732b6b2e4f127fafec0498cca11d44a41dc7da17
Hernando Dio Palma <hernando.diopalma@gmail.com>
2023-10-26 08:17:09 +0700

    Database: Modify Doa, Hadist, Ibadah Harian and Tahfidz table

--- database/seeders/Tahfidz1Seeder.php
+++ database/seeders/Tahfidz1Seeder.php
@@ -17,14 +17,59 @@ class Tahfidz1Seeder extends Seeder
      */
     public function run()
     {
-        for ($i = 0; $i <= 100; $i++)
-        {
+        $suratJuz30 = [
+            'Al-Fatihah',
+            'An-Naba',
+            'An-Nazi\'at',
+            'Abasa',
+            'At-Takwir',
+            'Al-Infitar',
+            'At-Tatfif',
+            'Al-Insyiqaq',
+            'Al-Buruj',
+            'At-Tariq',
+            'Al-A\'la',
+            'Al-Gasyiyah',
+            'Al-Fajr',
+            'Al-Balad',
+            'Asy-Syams',
+            'Al-Lail',
+            'Ad-Duha',
+            'Asy-Syarh',
+            'At-Tin',
+            'Al-\'Alaq',
+            'Al-Qadr',
+            'Al-Bayyinah',
+            'Az-Zalzalah',
+            'Al-\'Adiyat',
+            'Al-Qari\'ah',
+            'At-Takasur',
+            'Al-Asr',
+            'Al-Humazah',
+            'Al-Fil',
+            'Quraisy',
+            'Al-Ma\'un',
+            'Al-Kausar',
+            'Al-Kafirun',
+            'An-Nasr',
+            'Al-Lahab',
+            'Al-Ikhlas',
+            'Al-Falaq',
+            'An-Nas'
+        ];
+        
+        for ($i=0; $i < count($suratJuz30); $i++) { 
             DB::table('tahfidzs_1')->insert([
-                'nama_nilai' => 'al-fatihah',
-                'penilaian_huruf_angka_id' => ($i-101)*-1,
+                'nama_nilai' => $suratJuz30[$i],
+                'penilaian_huruf_angka_id' => mt_rand(1,100),
                 'nilai' => $i,
                 'guru_id' => mt_rand(1,10),
+                'created_at' => now(),
+                'updated_at' => now()
             ]);
         }
+
+
+        
     }
 }
