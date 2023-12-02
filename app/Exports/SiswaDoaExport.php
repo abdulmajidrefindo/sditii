27e7f48cea37f2e5f6e410049f25bdc289873c35
Hernando Dio Palma <hernando.diopalma@gmail.com>
2023-12-03 02:47:26 +0700

    Update cara export file dan tambah rule pengeditan file excel.

--- app/Exports/SiswaDoaExport.php
+++ app/Exports/SiswaDoaExport.php
@@ -7,39 +7,89 @@ use App\Models\SiswaDoa;
 use App\Models\Periode;
 use App\Models\SubKelas;
 
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
 
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithStyles;
+use Maatwebsite\Excel\Concerns\WithCustomStartCell;
 use PhpOffice\PhpSpreadsheet\Style\Protection;
 
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use PhpOffice\PhpSpreadsheet\Cell\DataValidation;
 
-class SiswaDoaExport implements FromCollection, ShouldAutoSize, WithHeadings, WithStyles
+class SiswaDoaExport implements FromView, WithStyles
 {
     /**
     * @return \Illuminate\Support\Collection
     */
 
-    private $row_lenght, $doa_column_length, $sub_kelas_id = 1;
+    private $row_lenght, $doa_column_length;
+    private $sub_kelas_id;
+    private $judul;
+    private $nama_kelas;
+    private $wali_kelas;
+    private $tahun_ajaran;
+    private $semester;
+    private $tanggal;
+    private $file_identifier;
 
-    public function __construct($sub_kelas_id)
+
+    public function __construct($sub_kelas_id, $informasi)
     {
         $this->sub_kelas_id = $sub_kelas_id;
+        $this->judul = $informasi['judul'];
+        $this->nama_kelas = $informasi['nama_kelas'];
+        $this->wali_kelas = $informasi['wali_kelas'];
+        $this->tahun_ajaran = $informasi['tahun_ajaran'];
+        $this->semester = $informasi['semester'];
+        $this->tanggal = $informasi['tanggal'];
+        $this->file_identifier = $informasi['file_identifier'];
     }
 
-    public function collection()
+
+    // public function collection()
+    // {
+    //     // return SiswaDoa::all();
+    //     $periode = Periode::where('status','aktif')->first();
+    //     $sub_kelas_id = $this->sub_kelas_id;
+
+    //     $siswa_d = SiswaDoa::with('siswa','doa_1','penilaian_huruf_angka')->where('periode_id',$periode->id)->whereHas('siswa', function ($query) use ($sub_kelas_id) {
+    //         $query->where('sub_kelas_id', $sub_kelas_id);
+    //     })->get();
+
+    //     $this->row_lenght = count($siswa_d);
+
+    //     $modified_siswa_d = $siswa_d->groupBy(['siswa_id'])->map(function ($item) {
+    //         $result = [];
+    //         $result['siswa_id'] = $item[0]->siswa_id;
+    //         $result['nama_siswa'] = $item[0]->siswa->nama_siswa;
+    //         $result['nisn'] = $item[0]->siswa->nisn;
+    //         foreach ($item as $doa_siswa) {
+    //             $result[$doa_siswa->doa_1->nama_nilai] = $doa_siswa->penilaian_huruf_angka->nilai_angka;
+    //         }
+    //         return $result;
+    //     });
+
+    //     return $modified_siswa_d;
+    // }
+
+    public function view(): View
     {
-        // return SiswaDoa::all();
         $periode = Periode::where('status','aktif')->first();
         $sub_kelas_id = $this->sub_kelas_id;
+        $kelas_id = SubKelas::where('id', $sub_kelas_id)->first()->kelas_id;
+        $data_doa = Doa1::where('kelas_id', $kelas_id)->where('periode_id', $periode->id)->get();
+        $doa_column_length = count($data_doa);
+        $this->doa_column_length = $doa_column_length;
 
         $siswa_d = SiswaDoa::with('siswa','doa_1','penilaian_huruf_angka')->where('periode_id',$periode->id)->whereHas('siswa', function ($query) use ($sub_kelas_id) {
             $query->where('sub_kelas_id', $sub_kelas_id);
         })->get();
 
-        $this->row_lenght = count($siswa_d);
+        
 
         $modified_siswa_d = $siswa_d->groupBy(['siswa_id'])->map(function ($item) {
             $result = [];
@@ -52,83 +102,61 @@ class SiswaDoaExport implements FromCollection, ShouldAutoSize, WithHeadings, Wi
             return $result;
         });
 
-        //column name
-        $column_name = [];
-        $column_name['siswa_id'] = 'ID data';
-        $column_name['nama_siswa'] = 'Nama Siswa';
-        $column_name['nisn'] = 'NISN';
-        foreach ($siswa_d as $doa_siswa) {
-            $column_name[$doa_siswa->doa_1->nama_nilai] = $doa_siswa->doa_1->nama_nilai;
-        }
+        $this->row_lenght = count($modified_siswa_d);
 
-        $collection = collect([$column_name]);
-        foreach ($modified_siswa_d as $siswa) {
-            $collection->push($siswa);
-        }
-
-        return $collection;
+        return view('siswaDoa.export_excel', [
+            'siswa_d' => $modified_siswa_d,
+            'judul' => $this->judul,
+            'nama_kelas' => $this->nama_kelas,
+            'wali_kelas' => $this->wali_kelas,
+            'tahun_ajaran' => $this->tahun_ajaran,
+            'semester' => $this->semester,
+            'tanggal' => $this->tanggal,
+            'file_identifier' => $this->file_identifier,
+            'column_length' => $this->doa_column_length,
+        ]);
     }
 
-    public function headings(): array
-    {
-
-        $title = 'Data Siswa Doa';
-        $tahun_ajaran = 'Tahun Ajaran 2020/2021';
-        $semester = 'Semester 1';
-        $tanggal = 'Tanggal '.date('d-m-Y');
-        $guru = 'Guru BK';
-
-        $kelas_id = SubKelas::where('id', $this->sub_kelas_id)->first()->kelas_id;
-
-        $header = [
-            'ID',
-            'Nama Siswa',
-            'NISN',
-            'DOA',
-            '', // Empty column for space for merge
-            '', // Empty column for space for merge
-            '', // Empty column for space for merge
-        ];
-
-        $periode = Periode::where('status','aktif')->first();
-
-        $data_doa = Doa1::where('kelas_id', $kelas_id)->where('periode_id', $periode->id)->get();
-        $this->doa_column_length = count($data_doa);
-
-        return [
-            [$title, $tahun_ajaran, $semester, $tanggal, $guru],
-            $header,
-        ];
-    }
-
+    //style overflow column
     public function styles(Worksheet $sheet)
     {
 
         // Enable worksheet protection
         $sheet->getParent()->getActiveSheet()->getProtection()->setSheet(true);
 
-        // Merge Cells for title
-        $sheet->mergeCells('A1:G1');
+        $sheet->getStyle('D11:' . $this->getColumnIndex($this->doa_column_length + 3) .'10')->getAlignment()->setWrapText(true);
+        $sheet->getStyle('D11:' . $this->getColumnIndex($this->doa_column_length + 3) .'10')->getAlignment()->setHorizontal('center');
+        $sheet->getStyle('D11:' . $this->getColumnIndex($this->doa_column_length + 3) .'10')->getAlignment()->setVertical('center');
+        $sheet->getStyle('D11:' . $this->getColumnIndex($this->doa_column_length + 3) .'10')->getAlignment()->setShrinkToFit(true);
 
-        // Merge Cells for header
-        $sheet->mergeCells('A2:A3'); // siswa_id
-        $sheet->mergeCells('B2:B3'); // nama_siswa
-        $sheet->mergeCells('C2:C3'); // nisn
-        $sheet->mergeCells('D2:'.$this->getColumnIndex($this->doa_column_length + 3).'2'); // Doa
+        //Unprotect nilai cell
+        $sheet->getStyle('D11:' . $this->getColumnIndex($this->doa_column_length + 3) . $this->row_lenght + 10)->getProtection()->setLocked(Protection::PROTECTION_UNPROTECTED);
 
-        // Set alignment for title
-        $sheet->getStyle('A1')->getAlignment()->setHorizontal('center');
-        $sheet->getStyle('A1')->getAlignment()->setVertical('center');
-
-        // Set alignment for header
-        $sheet->getStyle('A2:'.$this->getColumnIndex($this->doa_column_length + 3).'2')->getAlignment()->setHorizontal('center');
-        $sheet->getStyle('A2:'.$this->getColumnIndex($this->doa_column_length + 3).'2')->getAlignment()->setVertical('center');
-
-        // Unprotect a nilai column and rows
-        $sheet->getStyle('D4:'.$this->getColumnIndex($this->doa_column_length + 3).($this->row_lenght + 3))->getProtection()->setLocked(Protection::PROTECTION_UNPROTECTED);
+        // Add border to range
+        $sheet->getStyle('A9:' . $this->getColumnIndex($this->doa_column_length + 3) . $this->row_lenght + 10)->getBorders()->getAllBorders()->setBorderStyle('thin');
 
 
-        
+        //validation rule for nilai cell as integer between 0-100 and not empty only
+        $startCell = 'D11'; // Starting cell for validation
+        $endCell = $this->getColumnIndex($this->doa_column_length + 3) . ($this->row_lenght + 10); // Ending cell for validation
+        $validationRange = $startCell . ':' . $endCell;
+        $validation = $sheet->getCell($startCell)->getDataValidation();
+        $validation->setType(DataValidation::TYPE_WHOLE);
+        $validation->setOperator(DataValidation::OPERATOR_BETWEEN);
+        $validation->setAllowBlank(false); 
+        $validation->setFormula1(0);
+        $validation->setFormula2(100);
+        $validation->setErrorStyle(DataValidation::STYLE_STOP);
+        $validation->setShowErrorMessage(true);
+        $validation->setErrorTitle('Input Salah');
+        $validation->setError('Nilai harus berupa angka antara 0-100');
+
+        // Set the range that we want to apply the validation rule to
+        $sheet->setDataValidation($validationRange, $validation);
+
+
+        //A2-A6 Auto width cell
+        $sheet->getColumnDimension('A')->setAutoSize(true);
 
     }
 
@@ -136,7 +164,4 @@ class SiswaDoaExport implements FromCollection, ShouldAutoSize, WithHeadings, Wi
     {
         return \PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex($index);
     }
-
-
-
 }
