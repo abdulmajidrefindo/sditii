0d7fd4d5ca9afc9e9cc9a6ed9b9f3e0cdbc8c612
Hernando Dio Palma <hernando.diopalma@gmail.com>
2023-12-09 06:17:59 +0700

    update showPeriode layout

--- resources/views/Periode/showPeriode.blade.php
+++ resources/views/Periode/showPeriode.blade.php
@@ -29,125 +29,135 @@
 @stop
 @section('content')
     <div class="row">
-        <div class="col-12 col-sm-12 col-md-6">
+        <div class="col-12 col-sm-12 col-md-12">
             <div class="card card-dark">
                 <div class="card-header border-transparent">
                     <h3 class="card-title">Detail Periode </h3>
                     <div class="card-tools">
                         <!-- button to edit page-->
 
-                        <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                            <i class="fas fa-minus"></i>
-                        </button>
-                        <button type="button" class="btn btn-tool" data-card-widget="remove">
-                            <i class="fas fa-times"></i>
-                        </button>
+                        <a href="{{ route('dataPeriode.index') }}" class="btn btn-sm btn-secondary float-right">
+                            <i class="fas fa-arrow-left"></i> Kembali
+                        </a>
                     </div>
                 </div>
+                <form id="form_periode">
                 <div class="card-body">
-                    <form id="form_periode">
-                    <div class="row">
-                        <div class="col-sm-12">
+                    
+                        <div class="row">
+                            <div class="col-sm-6 border-right">
 
-                            <div class="form-group col-md-12">
-                                <label class="text-lightdark">
-                                    ID Periode
-                                </label>
-                                <div class="input-group">
-                                    <input id="id" name="id" value="{{ $dataPeriode->id }}" class="form-control"
-                                        disabled>
-                                </div>
-                            </div>
 
-                            <div class="form-group col-md-12">
-                                <label for="semester" class="text-lightdark">
-                                    Semester
-                                </label>
-                                <div class="input-group">
-                                    <input id="semester" name="semester" value="{{ $dataPeriode->semester }}"
-                                        class="form-control @error('semester') is-invalid @enderror" disabled>
-                                    @error('semester')
-                                        <div class="invalid-feedback">
-                                            {{ $message }}
-                                        </div>
-                                    @enderror
-                                </div>
-                            </div>
 
-                            <div class="form-group col-md-12">
-                                <label for="tahun_ajaran" class="text-lightdark">
-                                    Tahun Ajaran
-                                </label>
-                                <div class="input-group">
-                                    <input id="tahun_ajaran" name="tahun_ajaran" value="{{ $dataPeriode->tahun_ajaran }}"
-                                        class="form-control @error('tahun_ajaran') is-invalid @enderror" disabled>
-                                    @error('tahun_ajaran')
-                                        <div class="invalid-feedback">
-                                            {{ $message }}
-                                        </div>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="form-group col-md-12">
-
-                                <label for="status" class="text-lightdark">
-                                    Status
-                                </label>
-                                @if ($dataPeriode->status == 'aktif')
+                                <div class="form-group col-md-12">
+                                    <label for="semester" class="text-lightdark">
+                                        Semester
+                                    </label>
                                     <div class="input-group">
-                                        <div id="status" name="status"
-                                            class="form-control">
-                                            Aktif
+                                        <input id="semester" name="semester" value="{{ $dataPeriode->semester }}"
+                                            class="form-control @error('semester') is-invalid @enderror" disabled>
+                                        @error('semester')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <div class="form-group col-md-12">
+                                    <label for="tahun_ajaran" class="text-lightdark">
+                                        Tahun Ajaran
+                                    </label>
+                                    <div class="input-group">
+                                        <input id="tahun_ajaran" name="tahun_ajaran"
+                                            value="{{ $dataPeriode->tahun_ajaran }}"
+                                            class="form-control @error('tahun_ajaran') is-invalid @enderror" disabled>
+                                        @error('tahun_ajaran')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <div class="form-group col-md-12">
+
+                                    <label for="status" class="text-lightdark">
+                                        Status
+                                    </label>
+                                    @if ($dataPeriode->status == 'aktif')
+                                        <div class="input-group">
+                                            <div id="status" name="status" class="form-control">
+                                                Aktif
+                                            </div>
                                         </div>
-                                    </div>
-                                @else
-                                <div class="custom-control custom-checkbox">
-                                    <input class="custom-control-input" type="checkbox" id="status" type="checkbox" value="aktif" 
-                                    {{ $dataPeriode->status == 'aktif' ? 'checked' : '' }} disabled>
-                                    <label for="status" class="custom-control-label">Atur Sebagai Semester Aktif</label>
-                                    </div>
-                            
-                                @error('status')
-                                    <div class="invalid-feedback">
-                                        {{ $message }}
-                                    </div>
-                                @enderror
-                                @endif
+                                    @else
+                                        <div class="custom-control custom-checkbox pt-2">
+                                            <input class="custom-control-input" type="checkbox" id="status"
+                                                type="checkbox" value="aktif"
+                                                {{ $dataPeriode->status == 'aktif' ? 'checked' : '' }} disabled>
+                                            <label for="status" class="custom-control-label">Atur Sebagai Semester
+                                                Aktif</label>
+                                        </div>
+
+                                        @error('status')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    @endif
+                                </div>
                             </div>
 
-                            <x-adminlte-input name="created_at" type="text" value="{{ $dataPeriode->created_at }}"
-                                label="Waktu Ditambahkan" fgroup-class="col-md-12" disabled>
-
-                                <x-slot name="prependSlot">
-                                    <div class="input-group-text bg-purple">
-                                        <i class="fas fa-calendar-alt"></i>
+                            <div class="col-sm-6">
+                                <div class="form-group col-md-12">
+                                    <label class="text-lightdark">
+                                        ID Periode
+                                    </label>
+                                    <div class="input-group">
+                                        <input id="id" name="id" value="{{ $dataPeriode->id }}"
+                                            class="form-control" disabled>
                                     </div>
-                                </x-slot>
+                                </div>
 
-                            </x-adminlte-input>
+                                <x-adminlte-input name="created_at" type="text" value="{{ $dataPeriode->created_at }}"
+                                    label="Waktu Ditambahkan" fgroup-class="col-md-12" disabled>
+    
+                                    <x-slot name="prependSlot">
+                                        <div class="input-group-text bg-gradient-green">
+                                            <i class="fas fa-calendar-alt"></i>
+                                        </div>
+                                    </x-slot>
+    
+                                </x-adminlte-input>
+    
+                            <x-adminlte-input name="updated_at" type="text" value="{{ $dataPeriode->updated_at }}"
+                                    label="Waktu Diperbaharui" fgroup-class="col-md-12" disabled>
+    
+                                    <x-slot name="prependSlot">
+                                        <div class="input-group-text bg-gradient-green">
+                                            <i class="fas fa-calendar-alt"></i>
+                                        </div>
+                                    </x-slot>
+    
+                                </x-adminlte-input>
 
-                        <x-adminlte-input name="updated_at" type="text" value="{{ $dataPeriode->updated_at }}"
-                                label="Waktu Diperbaharui" fgroup-class="col-md-12" disabled>
+                            </div>
 
-                                <x-slot name="prependSlot">
-                                    <div class="input-group-text bg-purple">
-                                        <i class="fas fa-calendar-alt"></i>
-                                    </div>
-                                </x-slot>
-
-                            </x-adminlte-input>
-
-                            <x-adminlte-button id="edit" class="btn bg-purple col-12 edit"
-                                label="Edit Data" icon="fas fa fa-fw fa-edit" />
-                            <x-adminlte-button id="simpan" class="btn bg-purple col-12 simpan" type="submit"
-                                label="Simpan Data" icon="fas fa fa-fw fa-save" hidden />
-                            <x-adminlte-button id="batal" class="btn bg-red col-12 cancel" label="Batal"
-                                icon="fas fa fa-fw fa-times" hidden />
                         </div>
-                    </div>
-                    </form>
+                    
                 </div>
+
+                <div class="card-footer">
+                    <x-adminlte-button id="edit" class="btn bg-gradient-green edit" label="Edit Data"
+                                    icon="fas fa fa-fw fa-edit" />
+                                <x-adminlte-button id="simpan" class="btn bg-gradient-green simpan" type="submit"
+                                    label="Simpan Data" icon="fas fa fa-fw fa-save" hidden />
+                                <x-adminlte-button id="batal" class="btn bg-gradient-maroon cancel" label="Batal"
+                                    icon="fas fa fa-fw fa-times" hidden />
+                </div>
+
+            </form>
             </div>
         </div>
     </div>
